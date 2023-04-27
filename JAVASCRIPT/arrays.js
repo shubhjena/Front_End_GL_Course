ab21827c42b5e1396f8b856de83b20d3fef6ead8
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-28 01:22:24 +0530

    Update arrays.js

--- JAVASCRIPT/arrays.js
+++ JAVASCRIPT/arrays.js
@@ -2,4 +2,12 @@ let selectedColors = ['red', 'blue'];
 console.log(selectedColors[0]);
 selectedColors[2] = "green";
 selectedColors[3] = 2;
+console.log(selectedColors);
 console.log(selectedColors.length);
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+console.log(matrix);
