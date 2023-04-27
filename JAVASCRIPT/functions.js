29ae3e4d424c32d62e6c75f46f7b30dda92596b0
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-27 21:08:58 +0530

    Create functions.js

---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,13 @@
+
+function greet(name,lastName){
+    console.log('Hello ' + name +" "+ lastName);
+}
+
+//calculating a value
+function square(number){
+    return number*number;
+}
+
+greet('John','Carter');
+greet('Gary', 'lee');
+console.log(square(2));
