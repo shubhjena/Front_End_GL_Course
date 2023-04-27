3afeb233b8fd4349bdce0c7904821d6249b469d7
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-27 21:09:01 +0530

    Create index.js

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+let myName = "Shubhahsihs Jena";
+console.log(myName);
+let age = 25;
+let person = {
+myName: 'Shubhashish',
+age: 25
+}
+person.myName = "Benedict";
+let selection = 'myName';
+person[selection] = 'Mary';
