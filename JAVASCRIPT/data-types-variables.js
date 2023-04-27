df48099a099e9059e2b59f7a1ba6ee54235ebeaa
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-04-27 21:08:46 +0530

    Create data-types-variables.js

---
+++ JAVASCRIPT/data-types-variables.js
@@ -0,0 +1,13 @@
+var x=1, y;
+// console.log(x);
+// console.log(y);
+// console.log(typeof 1.5);
+// console.log(typeof x);
+// console.log(typeof y);
+myName = "Shubhashish", message = 'Good morning';
+// console.log(message+' '+ myName)
+console.log(typeof myName.length)
+
+var a = "1", b = "2";
+
+console.log( a + b );
